8e08e365db038d4f4e3a10620d529d93391e15b6
David Sandburg <20085982@K2.local>
2024-04-08 12:30:40 -0400

    added some folders for organization

--- main.py
+++
@@ -1,6 +0,0 @@
-import constants as c
-import pygame as py
-
-import game_example
-
-print(c.BOOLEAN)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+# regular imports
+import src.constants as c
+import pygame as py
+
+# games are imported here
+import games.game_example as game_example
+
+print(c.Example.BOOLEAN)
